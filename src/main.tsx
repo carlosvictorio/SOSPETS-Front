23bd8289984c9fc37daac70de9037bd6095b5e78
Carlos Victorio <victorionogueira@email.com>
2026-02-11 20:39:35 -0300

    feat: add route for volunteer page

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home/index.tsx";
 import Contact from "./pages/Contacts/index.tsx";
+import Volunteer from "./pages/Volunteer/index.tsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/contact", element: <Contact /> },
+  { path: "/beavolunteer", element: <Volunteer /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
